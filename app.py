8f8ac2b0b4a1e0866520a1295522ceeff5712e60
emunozlorenzo <emunozlorenzo@gmail.com>
2023-12-12 10:50:01 +0100

    Dates

--- app.py
+++ app.py
@@ -47,38 +47,42 @@ if selected == 'P&L':
 
     my_bar = st.progress(0, text="Collecting Data from Banco de España")
 
-    # Estado Financiero
-    get2 = requests.get(f'https://www.bde.es/app/sif/documentosAsociaciones/periodos/{dates[1]}/select2-estados-es.json')
-    pnl = [i['id'] for i in get2.json()['results'][1]['children'] if i['text'] == 'Cuenta de pérdidas y ganancias consolidada pública'][0]
-    url = f'https://www.bde.es/app/sif/documentosAsociaciones/periodos/{dict_dates[st.session_state.selector]}/{pnl}/{pnl}_{dict_dates[st.session_state.selector]}.xls'
+    try:
+        # Estado Financiero
+        get2 = requests.get(f'https://www.bde.es/app/sif/documentosAsociaciones/periodos/{dates[1]}/select2-estados-es.json')
+        pnl = [i['id'] for i in get2.json()['results'][1]['children'] if i['text'] == 'Cuenta de pérdidas y ganancias consolidada pública'][0]
+        url = f'https://www.bde.es/app/sif/documentosAsociaciones/periodos/{dict_dates[st.session_state.selector]}/{pnl}/{pnl}_{dict_dates[st.session_state.selector]}.xls'
 
-    my_bar.progress(75, text='Collecting Data from Banco de España')
-    output = src.pnl_all(url)
-    my_bar.empty()
-    
-    st.dataframe(output.style.applymap(lambda _: "background-color: #002D62; color: white; font-weight: bold", subset=([0,6,11,15,17,19], slice(None))),
-                 height=35*len(output)+38,
-                 width = 700,
-                 hide_index=True)
+        my_bar.progress(75, text='Collecting Data from Banco de España')
+        output = src.pnl_all(url)
+        my_bar.empty()
+        
+        st.dataframe(output.style.applymap(lambda _: "background-color: #002D62; color: white; font-weight: bold", subset=([0,6,11,15,17,19], slice(None))),
+                    height=35*len(output)+38,
+                    width = 700,
+                    hide_index=True)
 
-    col1, col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
-    with col1:
-        @st.cache_data
-        def convert_df(df):
-            return df.to_csv(index=False, sep=';').encode('utf-8')
+        with col1:
+            @st.cache_data
+            def convert_df(df):
+                return df.to_csv(index=False, sep=';').encode('utf-8')
 
-        csv = convert_df(output)
+            csv = convert_df(output)
 
-        st.download_button("Download DataFrame",
-                           csv,
-                           "file.csv",
-                           "text/csv",
-                           key='download-csv')
+            st.download_button("Download DataFrame",
+                            csv,
+                            "file.csv",
+                            "text/csv",
+                            key='download-csv')
 
-    with col2:
-        st.markdown(f'<a href="{url}" style="display: inline-block; padding: 5px 10px; background-color: #fffff; color: #454752; text-align: center; text-decoration: none; font-size: 16px; border-radius: 8px; border: 1px solid #e0e0e2;">Download Full Data</a>',
-                    unsafe_allow_html=True)
+        with col2:
+            st.markdown(f'<a href="{url}" style="display: inline-block; padding: 5px 10px; background-color: #fffff; color: #454752; text-align: center; text-decoration: none; font-size: 16px; border-radius: 8px; border: 1px solid #e0e0e2;">Download Full Data</a>',
+                        unsafe_allow_html=True)
+    except:
+        message = st.chat_message("assistant")
+        message.write("Hello human")    
         
 elif selected == "+Banks":
         selector = st.selectbox("Select a Date",
