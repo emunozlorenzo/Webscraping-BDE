217db70a5da2ee76ec432cfd53c582bc6954c063
emunozlorenzo <emunozlorenzo@gmail.com>
2024-02-14 16:34:21 +0100

    Fix Relative Path navbar.py

--- navbar.py
+++ navbar.py
@@ -3,8 +3,8 @@ from streamlit_option_menu import option_menu
 
 # Define the pages and their file paths
 pages = {'Stocks':'app.py',
-         'P&L':'pages/p&l.py',
-         '+Banks':'pages/banks.py'}
+         'P&L':'p&l.py',
+         '+Banks':'banks.py'}
 
 # Create a list of the page names
 page_list = list(pages.keys())
@@ -22,6 +22,10 @@ def nav(current_page=page_list[0]):
         "nav-link-selected": {"font-size": "14px", "background-color": "#072146"}})
 
     if current_page != p:
-        st.switch_page(pages[p])
+        if p == 'Stocks':
+            path_to_page = os.path.join(os.getcwd(), "app.py")
+        else:
+            path_to_page = os.path.join(os.getcwd(), "pages", pages[p])
+        st.switch_page(path_to_page)
 
 
